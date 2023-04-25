c349f98314ccfca515907fe2abf61261c4f5bf46
sasha <sasha.laigle2022@campus-eni.fr>
2023-04-25 09:30:13 +0200

    :recycle: Clean code ajout des methodes pour le contenu des bindings (articleView)

--- app/src/main/java/com/example/cado/ui/articleview/ArticleViewFragment.kt
+++ app/src/main/java/com/example/cado/ui/articleview/ArticleViewFragment.kt
@@ -1,21 +1,23 @@
 package com.example.cado.ui.articleview
 
 import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.cado.R
 import com.example.cado.bo.Article
 import com.example.cado.databinding.FragmentArticleViewBinding
 import com.example.cado.repository.ArticleRepository
-import com.google.android.material.snackbar.Snackbar
 import java.time.LocalDate
 
 
@@ -56,12 +58,9 @@ class ArticleViewFragment : Fragment() {
 
 
         binding.imageButtonInternet.setOnClickListener {
-            val toast = Toast.makeText(
-                context,
-                "Page Internet disponible : " + currentArticle.url,
-                Toast.LENGTH_SHORT)
-            toast.show()
+            accederUrlArticle()
         }
+
 
 
         binding.imageButtonEdit.setOnClickListener {
@@ -84,12 +83,7 @@ class ArticleViewFragment : Fragment() {
 
 
         binding.imageButtonSMS.setOnClickListener {
-            val snack = Snackbar.make(it,"confirmer l'envoi du sms ?", Snackbar.LENGTH_LONG)
-            snack.setAction("DISMISS") {
-                // executed when DISMISS is clicked
-                Log.i("Snackbar", "Snackbar Set Action - OnClick.")
-            }
-            snack.show()
+            envoyerSms()
         }
 
 
@@ -101,6 +95,56 @@ class ArticleViewFragment : Fragment() {
             displayData()
         }
     }
+
+    private fun envoyerSms() {
+        var numero = "0666666666"
+        val intent = Intent(Intent.ACTION_SENDTO)
+        try {
+            intent.setData(Uri.parse("smsto:$numero"))
+            intent.putExtra("sms_body", "Idée de cadeau : " + currentArticle.url)
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            Log.i("EnvoiSms", "Erreur lors de l'envoi du sms")
+            val toast = Toast.makeText(
+                context,
+                "Erreur lors de l'envoi du sms",
+                Toast.LENGTH_SHORT
+            )
+            toast.show()
+
+        }
+    }
+
+    private fun accederUrlArticle() {
+        try {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(currentArticle.url)))
+        } catch (e: ActivityNotFoundException) {
+            val toast = Toast.makeText(
+                context,
+                "Url du cadeau : " + currentArticle.url,
+                Toast.LENGTH_SHORT
+            )
+            toast.show()
+        }
+    }
+
+/*    private fun modifierArticle() {
+        val builder = AlertDialog.Builder(context)
+        builder.setMessage("Etes vous sûr de vouloir modifier cet article ?")
+            .setCancelable(true)
+            .setPositiveButton("Oui") { dialog, id ->
+//                    val action = ArticleViewFragmentDirections
+                val action = ArticleViewFragmentDirections.actionArticleViewFragmentToArticleEditFragment(currentArticle)
+                Navigation.findNavController(it).navigate(action)
+            }
+            .setNegativeButton("Non") { dialog, id ->
+                // Dismiss the dialog
+                dialog.dismiss()
+            }
+        val alert = builder.create()
+        alert.show()
+
+    }*/
 
     private fun displayData() {
         binding.article = currentArticle
